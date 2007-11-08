7b91b9f3813368df7fecc000fe75476da7aa2e1c
nicwilliams <nicwilliams@b8dfb574-5f1f-0410-9469-b441dc4e948f>
2007-11-08 03:21:10 +0000

    Cleaned the \n\n for History comments in generated hoe.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/newgem/trunk@132 b8dfb574-5f1f-0410-9469-b441dc4e948f

--- app_generators/newgem/templates/config/hoe.rb
+++ app_generators/newgem/templates/config/hoe.rb
@@ -55,10 +55,10 @@ hoe = Hoe.new(GEM_NAME, VERS) do |p|
   p.url = HOMEPATH
   p.rubyforge_name = RUBYFORGE_PROJECT if RUBYFORGE_PROJECT
   p.test_globs = ["test/**/test_*.rb"]
-  p.clean_globs |= ['**/.*.sw?', '*.gem', '.config', '**/.DS_Store']  #An array of file patterns to delete on clean.
+  p.clean_globs |= ['**/.*.sw?', '*.gem', '.config', '**/.DS_Store', '.git*']  #An array of file patterns to delete on clean.
   
   # == Optional
-  p.changes = p.paragraphs_of("History.txt", 0..1).join("\\n\\n")
+  p.changes = p.paragraphs_of("History.txt", 0..1).join("\n\n")
   #p.extra_deps = []     # An array of rubygem dependencies [name, version], e.g. [ ['active_support', '>= 1.3.1'] ]
   <% if is_jruby %>
   # JRuby gem created, e.g. <%= gem_name %>-X.Y.Z-jruby.gem
