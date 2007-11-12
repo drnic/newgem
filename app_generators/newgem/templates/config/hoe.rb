e179ceb33d6de8934912ab7cfb94c7892bd74897
nicwilliams <nicwilliams@b8dfb574-5f1f-0410-9469-b441dc4e948f>
2007-11-12 23:02:20 +0000

    == 0.16.1 2007-11-13
    * Oops, removed .git* from the hoe clean list as that blows away the .git folder...
    
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/newgem/trunk@138 b8dfb574-5f1f-0410-9469-b441dc4e948f

--- app_generators/newgem/templates/config/hoe.rb
+++ app_generators/newgem/templates/config/hoe.rb
@@ -55,7 +55,7 @@ hoe = Hoe.new(GEM_NAME, VERS) do |p|
   p.url = HOMEPATH
   p.rubyforge_name = RUBYFORGE_PROJECT if RUBYFORGE_PROJECT
   p.test_globs = ["test/**/test_*.rb"]
-  p.clean_globs |= ['**/.*.sw?', '*.gem', '.config', '**/.DS_Store', '.git*']  #An array of file patterns to delete on clean.
+  p.clean_globs |= ['**/.*.sw?', '*.gem', '.config', '**/.DS_Store']  #An array of file patterns to delete on clean.
   
   # == Optional
   p.changes = p.paragraphs_of("History.txt", 0..1).join("\n\n")
