0c31f2857bf4941053227cd98d2100a0b7a1acb6
Dr Nic <drnicwilliams@gmail.com>
2008-03-27 06:51:28 +1000

    regen website

--- website/version-raw.js
+++ website/version-raw.js
@@ -1,7 +1,3 @@
 // Announcement JS file
-<<<<<<< HEAD:website/version-raw.js
 var version = "0.19.1";
-=======
-var version = "0.18.0";
->>>>>>> 5a73b7c53a5cbce9c18bea0801f7eb165481a7bb:website/version-raw.js
 MagicAnnouncement.show('compositekeys', version);
