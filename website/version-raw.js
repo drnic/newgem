60df2b95ce0360f816cdabaa97a36525969f3ff4
nicwilliams <nicwilliams@b8dfb574-5f1f-0410-9469-b441dc4e948f>
2008-03-31 14:40:47 +0000

    0.20.1
    git-svn-id: svn+ssh://rubyforge.org/var/svn/newgem/trunk@169 b8dfb574-5f1f-0410-9469-b441dc4e948f

--- website/version-raw.js
+++ website/version-raw.js
@@ -1,3 +1,3 @@
 // Announcement JS file
-var version = "0.20.0";
+var version = "0.20.1";
 MagicAnnouncement.show('compositekeys', version);
--- website/version.js
+++ website/version.js
@@ -1,4 +1,4 @@
 // Version JS file
-var version = "0.20.0";
+var version = "0.20.1";
 
 document.write(" - " + version);
