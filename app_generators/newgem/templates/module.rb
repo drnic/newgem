03042822c129de66a44fbfcb3318fab16a556523
Dr Nic <drnicwilliams@gmail.com>
2008-04-02 19:26:14 +0200

    module.rb - more advanced $:.unshift sequence/cmd

--- app_generators/newgem/templates/module.rb
+++ app_generators/newgem/templates/module.rb
@@ -1,4 +1,5 @@
-$:.unshift File.dirname(__FILE__)
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module <%= module_name %>
   
