63275ec9bbc67226b7cab66b96f8dbcfb96826de
Dr Nic Williams <drnicwilliams@gmail.com>
2008-04-09 08:53:32 +1000

    0.22.2 release

--- website/version-raw.js
+++ website/version-raw.js
@@ -1,3 +1,3 @@
 // Announcement JS file
-var version = "0.22.1";
+var version = "0.22.2";
 MagicAnnouncement.show('compositekeys', version);
--- website/version.js
+++ website/version.js
@@ -1,4 +1,4 @@
 // Version JS file
-var version = "0.22.1";
+var version = "0.22.2";
 
 document.write(" - " + version);
