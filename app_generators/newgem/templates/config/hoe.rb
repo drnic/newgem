47524bdbeb210cfc28304262eb1794b4fd21024f
Dr Nic Williams <drnicwilliams@gmail.com>
2008-10-21 06:21:52 +1300

    cleaning up hoe.rb

--- app_generators/newgem/templates/config/hoe.rb
+++ app_generators/newgem/templates/config/hoe.rb
@@ -8,6 +8,7 @@ GEM_NAME           = '<%= gem_name %>' # what ppl will type to install your gem
 RUBYFORGE_PROJECT  = '<%= project_name %>' # The unix name for your project
 HOMEPATH           = 'http://#{RUBYFORGE_PROJECT}.rubyforge.org'
 DOWNLOAD_PATH      = 'http://rubyforge.org/projects/#{RUBYFORGE_PROJECT}'
+RUBYFORGE_USERNAME = ENV['RUBYFORGE_USERNAME'] || 'unknown'
 EXTRA_DEPENDENCIES = [
 #  ['activesupport', '>= 1.3.1']
 ]    # An array of rubygem dependencies [name, version]
@@ -15,36 +16,18 @@ EXTRA_DEV_DEPENDENCIES = [
 #  ['rspec', '>= 1.1.5']
 ]    # An array of rubygem dependencies [name, version]
 
-@config_file = "~/.rubyforge/user-config.yml"
-@config = nil
-RUBYFORGE_USERNAME = "unknown"
-def rubyforge_username
-  unless @config
-    begin
-      @config = YAML.load(File.read(File.expand_path(@config_file)))
-    rescue
-      puts <<-EOS
-ERROR: No rubyforge config file found: #{@config_file}
-Run 'rubyforge setup' to prepare your env for access to Rubyforge
- - See http://newgem.rubyforge.org/rubyforge.html for more details
-      EOS
-      exit
-    end
-  end
-  RUBYFORGE_USERNAME.replace @config["username"]
-end
 
 # Generate all the Rake tasks
 # Run 'rake -T' to see list of generated tasks (from gem root directory)
 $hoe = Hoe.new(GEM_NAME, VERS = <%= module_name %>::VERSION::STRING) do |p|
   p.developer(AUTHOR, EMAIL)
-  p.description    = DESCRIPTION
-  p.summary        = DESCRIPTION
-  p.url            = HOMEPATH
-  p.rubyforge_name = RUBYFORGE_PROJECT if RUBYFORGE_PROJECT
-  p.test_globs     = ["test/**/test_*.rb"]
-  p.clean_globs |= ['**/.*.sw?', '*.gem', '.config', '**/.DS_Store']  #An array of file patterns to delete on clean.
+  p.description          = DESCRIPTION
+  p.summary              = DESCRIPTION
+  p.url                  = HOMEPATH
+  p.rubyforge_name       = RUBYFORGE_PROJECT if RUBYFORGE_PROJECT
   p.post_install_message = 'PostInstall.txt'
+  p.test_globs           = ["test/**/test_*.rb"]
+  p.clean_globs |= ['**/.*.sw?', '*.gem', '.config', '**/.DS_Store', 'tmp']  # An array of file patterns to delete on clean.
 
   # == Optional
   p.changes        = p.paragraphs_of("History.txt", 0..1).join("\n\n")
@@ -52,15 +35,11 @@ $hoe = Hoe.new(GEM_NAME, VERS = <%= module_name %>::VERSION::STRING) do |p|
   p.extra_dev_deps = EXTRA_DEV_DEPENDENCIES
 
   <% if is_jruby -%>
-  # JRuby gem created, e.g. <%= gem_name %>-X.Y.Z-jruby.gem
-  p.spec_extras = { :platform => 'jruby' }    # A hash of extra values to set in the gemspec.
-  <% else -%>
-  p.spec_extras = {}    # A hash of extra values to set in the gemspec.
+  p.spec_extras['platform'] = 'jruby' # JRuby gem created, e.g. <%= gem_name %>-X.Y.Z-jruby.gem
   <% end -%>
+  p.spec_extras['rdoc_options'] = ['--main', Dir['README*'].first]
 end
-$hoe.spec.rdoc_options = ['--main', Dir['README*'].first]
 
-CHANGES = $hoe.paragraphs_of('History.txt', 0..1).join("\\n\\n")
 PATH    = (RUBYFORGE_PROJECT == GEM_NAME) ? RUBYFORGE_PROJECT : "#{RUBYFORGE_PROJECT}/#{GEM_NAME}"
 $hoe.remote_rdoc_dir = File.join(PATH.gsub(/^#{RUBYFORGE_PROJECT}\/?/,''), 'rdoc')
 $hoe.rsync_args = '-av --delete --ignore-errors'
--- config/hoe.rb
+++ config/hoe.rb
@@ -2,57 +2,38 @@ require 'hoe'
 require 'newgem/version'
 require 'newgem/support/tasks'
 
-AUTHOR            = "Dr Nic Williams"
-EMAIL             = "drnicwilliams@gmail.com"
-DESCRIPTION       = "Make your own gems at home"
-GEM_NAME          = "newgem" # what ppl will type to install your gem
-RUBYFORGE_PROJECT = "newgem"
-HOMEPATH          = "http://#{RUBYFORGE_PROJECT}.rubyforge.org"
-DOWNLOAD_PATH     = "http://rubyforge.org/projects/#{RUBYFORGE_PROJECT}"
-
-@config_file = "~/.rubyforge/user-config.yml"
-@config = nil
-RUBYFORGE_USERNAME = ENV['RUBYFORGE_USERNAME']
-def rubyforge_username
-  unless @config
-    begin
-      @config = YAML.load(File.read(File.expand_path(@config_file)))
-    rescue
-      puts <<-EOS
-ERROR: No rubyforge config file found: #{@config_file}
-Run 'rubyforge setup' to prepare your env for access to Rubyforge
- - See http://newgem.rubyforge.org/rubyforge.html for more details
-      EOS
-      exit
-    end
-  end
-  RUBYFORGE_USERNAME.replace(@config["username"] || "unknown")
-end
+AUTHOR             = "Dr Nic Williams"
+EMAIL              = "drnicwilliams@gmail.com"
+DESCRIPTION        = "Make your own gems at home"
+GEM_NAME           = "newgem" # what ppl will type to install your gem
+RUBYFORGE_PROJECT  = "newgem"
+HOMEPATH           = "http://#{RUBYFORGE_PROJECT}.rubyforge.org"
+DOWNLOAD_PATH      = "http://rubyforge.org/projects/#{RUBYFORGE_PROJECT}"
+RUBYFORGE_USERNAME = ENV['RUBYFORGE_USERNAME'] || 'unknown'
 
 CLEAN.include ['**/.*.sw?', '*.gem', '.config', '**/.DS_Store', 'tmp', '*.log']
 
 # Generate all the Rake tasks
 # Run 'rake -T' to see list of generated tasks (from gem root directory)
-hoe = Hoe.new(GEM_NAME, VERS = Newgem::VERSION::STRING) do |p|
+$hoe = Hoe.new(GEM_NAME, VERS = Newgem::VERSION::STRING) do |p|
   p.developer(AUTHOR, EMAIL)
-  p.description    = DESCRIPTION
-  p.summary        = DESCRIPTION
-  p.url            = HOMEPATH
-  p.rubyforge_name = RUBYFORGE_PROJECT if RUBYFORGE_PROJECT
-  p.test_globs     = ["test/**/test*.rb"]
+  p.description          = DESCRIPTION
+  p.summary              = DESCRIPTION
+  p.url                  = HOMEPATH
+  p.rubyforge_name       = RUBYFORGE_PROJECT if RUBYFORGE_PROJECT
+  p.test_globs           = ["test/**/test*.rb"]
   p.post_install_message = 'PostInstall.txt'
-  p.changes = p.paragraphs_of("History.txt", 0..1).join("\n\n")
-  p.extra_deps = [
+  p.changes              = p.paragraphs_of("History.txt", 0..1).join("\n\n")
+  p.extra_deps           = [
     ['RedCloth','>=4.0.0'],
     ['syntax','>=1.0.0'],
     ['activesupport','>=2.0.2'],
     ['rubigen','>=1.3.3'],
     ['hoe', '>=1.8.0']
   ]
+  p.spec_extras['rdoc_options'] = ['--main', Dir['README*'].first]
 end
-hoe.spec.rdoc_options = ['--main', Dir['README*'].first]
 
-CHANGES = hoe.paragraphs_of('History.txt', 0..1).join("\n\n")
 PATH    = (RUBYFORGE_PROJECT == GEM_NAME) ? RUBYFORGE_PROJECT : "\#{RUBYFORGE_PROJECT}/\#{GEM_NAME}"
-hoe.remote_rdoc_dir = File.join(PATH.gsub(/^#{RUBYFORGE_PROJECT}\/?/,''), 'rdoc')
-hoe.rsync_args = '-av --delete --ignore-errors'
+$hoe.remote_rdoc_dir = File.join(PATH.gsub(/^#{RUBYFORGE_PROJECT}\/?/,''), 'rdoc')
+$hoe.rsync_args = '-av --delete --ignore-errors'
