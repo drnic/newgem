4a19ada3564d2ba3b50e4e14e56d298f33e72ac0
Dr Nic Williams <drnicwilliams@gmail.com>
2008-10-26 19:32:31 +1000

    remove old rails cucumber generator cruft from env.rb

--- rubygems_generators/install_cucumber/templates/env.rb
+++ rubygems_generators/install_cucumber/templates/env.rb
@@ -4,15 +4,3 @@ gem 'cucumber'
 require "cucumber"
 gem 'rspec'
 require "spec"
-
-# Following lines from original Rails cucumber generator. 
-# Not sure how to translate/reuse etc yet.
-# 
-# # Sets up the Rails environment for Cucumber
-# ENV["RAILS_ENV"] = "test"
-# require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
-# require 'cucumber/rails/world'
-# Cucumber::Rails.use_transactional_fixtures
-# 
-# # Comment out the next line if you're not using RSpec's matchers (should / should_not) in your steps.
-# require 'cucumber/rails/rspec'
