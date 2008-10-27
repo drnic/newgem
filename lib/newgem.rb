a4bbe35d39d9e7695b1b843ae9bf8cc10b3a7aad
Dr Nic Williams <drnicwilliams@gmail.com>
2008-10-28 09:46:38 +1000

    prep for 1.0.1 release

--- lib/newgem.rb
+++ lib/newgem.rb
@@ -2,5 +2,5 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Newgem
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 end
