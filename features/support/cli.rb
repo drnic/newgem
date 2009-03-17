d8564a58bcab7c3c065d499637ca042afda0fea1
Dr Nic Williams <drnicwilliams@gmail.com>
2009-03-17 12:01:08 +0800

    cucumber support files use modules instead of polluting root namespace

--- features/support/cli.rb
+++ features/support/cli.rb
@@ -1,3 +1,8 @@
-def newgem_cmd
-  File.expand_path(File.dirname(__FILE__) + "/../../bin/newgem")
+module CliHelpers
+  def newgem_cmd
+    File.expand_path(File.dirname(__FILE__) + "/../../bin/newgem")
+  end
 end
+
+World { |world| world.extend CliHelpers }
+
